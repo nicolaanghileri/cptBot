37a82daf7ef7c3de8e88ebe106fe713fdaa81572
IsmaelTrentin <ismael.trentin@samtrevano.ch>
2020-05-07 17:30:12 +0200

    Update toStr

--- util/logging.js
+++ util/logging.js
@@ -1,10 +1,14 @@
+const aHelper = require('./../helpers/array-helper');
+
 const SUCCESS = "SUC";
 const INFORMATION = "INF";
 const WARNING = "WAR";
 const ERROR = "ERR";
 
 const BOT = " BOT ";
-const CLIENT = "GUILD"
+const CLIENT = "GUILD";
+
+
 
 /**
  * Improved version of console.log().
@@ -24,9 +28,31 @@ function log(msg, category, side) {
     console.log(`[${h}:${m}:${s}][${side}][${category}] ${msg}`);
 }
 
+/**
+ * Logs informations about the cmdName command execution.
+ * 
+ * @param {User} author the discord message author
+ * @param {Object} cmdOutput the command output object
+ * @param {string} cmdName the command name
+ */
+function logCommand(author, cmdOutput = {}, cmdName, args) {
+    let username = author.username;
+    let suc = (cmdOutput.success) ? SUCCESS : ERROR;
+    let content;
+    if (cmdOutput.success)
+        content = `Executed ${cmdName} successfully with parameters: ${aHelper.toStr(args)}`;
+    else
+        content = cmdOutput.cause;
+    let data = new Date();
+    let h = (data.getHours().toString().length == 1) ? "0" + data.getHours() : data.getHours();
+    let m = (data.getMinutes().toString().length == 1) ? "0" + data.getMinutes() : data.getMinutes();
+    let s = (data.getSeconds().toString().length == 1) ? "0" + data.getSeconds() : data.getSeconds();
+    console.log(`[${h}:${m}:${s}][GUILD][${suc}][@${username}] ${content}`);
+}
 
 module.exports = {
     log,
+    logCommand,
     BOT,
     CLIENT,
     SUCCESS,
