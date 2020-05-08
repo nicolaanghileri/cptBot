8d4aba28c98dcde3749b2aa50ed5d41736c0a718
IsmaelTrentin <ismael.trentin@samtrevano.ch>
2020-05-08 17:13:45 +0200

    Add testing commands

--- commands/names.js
+++ commands/names.js
@@ -1,9 +1,11 @@
 const TEST = 'test';
 const NEW_TEACHER = 'newteacher';
 const NEW_STUDENT = 'newstudent';
+const NEW_MODULE = 'newmodule';
 
 module.exports = {
     TEST,
     NEW_TEACHER,
-    NEW_STUDENT
+    NEW_STUDENT,
+    NEW_MODULE
 };
