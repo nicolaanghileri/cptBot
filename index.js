398102635641cf6582969d1bc1205c20d9e0dac8
IsmaelTrentin <ismael.trentin@samtrevano.ch>
2020-05-13 19:13:25 +0200

    Update index

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cmdNames = require('./commands/names');
 
 //inits
 var logger = new Logger();
-var jtor = new Jsonator('./data/students.json', './data/teachers.json', './data/modulez.json');
+var jtor = new Jsonator('./data/students.json', './data/teachers.json', './data/modulez.json', './data/timetables/');
 var gHelper = new GuildHelper();
 var lHelper = new ListHelper();
 
@@ -25,48 +25,6 @@ bot.on('ready', () => {
     logger.log(`Connected Guilds(${names.length}): ${lHelper.toStr(names)}`);
     logger.log(`Loaded commands: ${lHelper.objToStr(cmdNames)}`);
     logger.log(`Prefix is: ${PREFIX}`);
-
-    /*
-    ser.addStudent('dsad', 'sur', 'AA', 2)
-        .then((s) => {
-            logger.log(`Added new student ${s.name} ${s.surname}.`, logger.categories.SUCCESS);
-        })
-        .catch(err => {
-            logger.log(err, logger.categories.ERROR);
-        });
-    let te;
-    ser.addTeacher('fasdfffsf', 'ffffff')
-        .then(t => {
-            logger.log(`Added new teacher ${t.name} ${t.surname}.`, logger.categories.SUCCESS);
-            ser.addMod('Mod ciao', 2, false, t)
-                .then(m => {
-                    logger.log(`Added new module ${m.name} - ${m.teacher.surname} ${m.teacher.name}.`, logger.categories.SUCCESS);
-                })
-                .catch(err => {
-                    logger.log(err, logger.categories.ERROR);
-                });
-        })
-        .catch(err => {
-            logger.log(err, logger.categories.ERROR);
-        });
-        
-
-    ser.getModsTeached('Francesco', 'Mussi')
-        .then(s => {
-            console.log(s);
-            //logger.log(`Added new module ${s.name} with teacher ${s.teacher.name} ${s.teacher.surname}`, logger.categories.SUCCESS);
-        })
-        .catch(err => {
-            logger.log(err, logger.categories.ERROR);
-        });*/
-    jtor.getMod('Mod. 10d5')
-        .then(t => {
-            console.log(t);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-
 });
 
 bot.on('message', msg => {
@@ -77,13 +35,6 @@ bot.on('message', msg => {
 
     switch (cmd) {
         case cmdNames.TEST:
-            /*    
-            gHelper.cloneCategory(msg.guild, args[0], args[1]);
-            let cat = msg.channel.parent;
-            cat.children.forEach(c => {
-                if(c.type === 'voice')
-                    gHelper.createTextFromVoiceChannel(msg.guild, c);
-            });*/
             break;
         case cmdNames.NEW_TEACHER:
             break;
